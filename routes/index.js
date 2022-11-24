8ec7b0db5fb5b8fb40491c33f4af7890caf9391d
caiozhang <caiozhang@tencent.com>
2022-11-24 16:12:04 +0800

    feat: init ts

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
-router.post('/api/info/get', function(req, res, next) {
-  
+router.post('/api/info/get', function (req, res, next) {
+    const operationid = req.body;
 });
-
 module.exports = router;
+//# sourceMappingURL=index.js.map
---
+++ routes_ts/index.ts
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/api/info/get', function(req, res, next) {
+  const operationid = req.body;
+  
+});
+
+module.exports = router;
