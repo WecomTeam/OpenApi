132573c9909b0b8e4ea199f9878484d9f6aa2bfc
caiozhang <caiozhang@tencent.com>
2022-11-24 15:47:33 +0800

    feat: 初始化编辑页面

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/api/info/get', function(req, res, next) {
+  
+});
 
 module.exports = router;
