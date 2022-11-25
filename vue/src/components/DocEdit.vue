b3bd925049ae57f77ba098f5da0c65326e6eadc9
meterscao <meterscao@gmail.com>
2022-11-25 16:32:46 +0800

    Change to another component with json editor vue

--- vue/src/components/DocEdit.vue
+++ vue/src/components/DocEdit.vue
@@ -1,57 +1,67 @@
 <template>
-    <div class="editor__wrapper">
-      <JsonEditor
+  <div class="editor__wrapper">
+    <!-- <JsonEditor
           v-model="schema"
           mode="code"
           :expanded-on-start="true"
-          :show-btns="false"
+          :show-btns="true"
           @json-save="onSave"
-          :main-menu-bar="false"
+          :main-menu-bar="false"          
+          :mainMenuBar="false"
           :navigation-bar="false"
           :status-bar="false"
           tabSize="4"
-      />
-    </div>
-  </template>
+      /> -->
+      
+        <JsonEditorVue class="jse-theme-dark" v-model="schema" :main-menu-bar="true" indentation=" " tabSize="4" mode="code" />
+      
+    
+  </div>
+</template>
   
-  <script>
-  import JsonEditor from 'vue-json-editor';
-  export default {
-    name: 'DocEdit',
-    components: {
-      JsonEditor
-    },
-    props: ['apiSchema'],
-    watch: {
-      apiSchema: {
-        handler(val) {
-          this.schema = val
-        },
-        deep: true
-      }
-    },
-    data() {
-      return {
-        schema: this.apiSchema
-      }
-    },
-    methods: {
-      onSave() {
-        this.$emit('save', this.schema)
-      }
+<script>
+
+import JsonEditorVue from 'json-editor-vue'
+
+export default {
+  name: 'DocEdit',
+  components: {
+
+    JsonEditorVue
+  },
+  props: ['apiSchema'],
+  watch: {
+    apiSchema: {
+      handler(val) {
+        this.schema = val
+      },
+      deep: true
+    }
+  },
+  data() {
+    return {
+      schema: this.apiSchema
+    }
+  },
+  methods: {
+    onSave() {
+      this.$emit('save', this.schema)
     }
   }
-  </script>
+}
+</script>
   
-  <style lang="less" scoped>
-  .editor__wrapper {
-    height: calc(100vh - 113px);
-    &>div {
-      height: 100%;
-    }
+<style lang="less" scoped>
+.editor__wrapper {
+  height: calc(100vh - 113px);
+
+  &>div {
+    height: 100%;
   }
-  /deep/.jsoneditor-vue {
-      height: calc(100% - 50px);
-    }
-  </style>
+}
+
+/deep/.jsoneditor-vue {
+  height: calc(100% - 50px);
+}
+</style>
   
