6ddb72933f74dd3b11cd93cf840e284ddcec5635
caiozhang <caiozhang@tencent.com>
2022-11-29 21:57:41 +0800

    feat: 接口数据改为yaml保存

--- logic/operation.js
+++ logic/operation.js
@@ -1,9 +1,9 @@
 
 const fs = require('fs');
 const path = require('path');
-
+const jsyaml = require('js-yaml')
 const parseValidFile = operationid => {
-    return path.join(__dirname, `../../api/${operationid.replace(/[\.\/\\]/g, '')}.json`)
+    return path.join(__dirname, `../../api/${operationid.replace(/[\.\/\\]/g, '')}.yaml`)
 }
 
 export const getApiSchema = async(operationid) => {
@@ -11,8 +11,11 @@ export const getApiSchema = async(operationid) => {
         const filePath = parseValidFile(operationid)
         const isExist = await fs.existsSync(filePath)
         if(isExist) {
-            const jsonData = await fs.readFileSync(filePath)
-            return JSON.parse(jsonData)
+            const yamlData = await fs.readFileSync(filePath)
+            return {
+                yaml: yamlData.toString(),
+                schema: jsyaml.load(yamlData)
+            }
         } else {
             return {}
         }
@@ -21,12 +24,12 @@ export const getApiSchema = async(operationid) => {
         return {}
     }
 }
-export const editApiSchema = async(operationid, schema) => {
+export const editApiSchema = async(operationid, yamlData) => {
     try {
     const filePath = parseValidFile(operationid)
     const isExist = await fs.existsSync(filePath)
     if(isExist) {
-        await fs.writeFileSync(filePath, JSON.stringify(schema))
+        await fs.writeFileSync(filePath, yamlData)
     } else {
         // 不存在暂不处理
     }
--- routes_ts/api.ts
+++ routes_ts/api.ts
@@ -10,18 +10,18 @@ import { getApiSchema, editApiSchema } from '../logic/operation.js'
 router.post('/info/get', async (req, res, next) => {
   const { operationid } = req.body;
 
-  const jsonData = await getApiSchema(operationid)
+  const {yaml, schema} = await getApiSchema(operationid)
   try {
-    const md = genWecomApiDoc(jsonData)
+    const md = genWecomApiDoc(schema)
     res.send({
-      schema: jsonData,
+      yaml,
       md
     })
   } catch (e) {
     console.log(`---- ${operationid} schema解析失败----`)
     console.error(e)
     res.send({
-      schema: jsonData,
+      yaml,
       md: {
         domStr: '',
         md: ''
@@ -31,20 +31,20 @@ router.post('/info/get', async (req, res, next) => {
 });
 
 router.post('/info/edit', async (req, res, next) => {
-  const { operationid, schema } = req.body
-  await editApiSchema(operationid, schema)
-  const jsonData = await getApiSchema(operationid)
+  const { operationid, yaml } = req.body
+  await editApiSchema(operationid, yaml)
+  const {newYaml, schema} = await getApiSchema(operationid)
   try {
-    const md = genWecomApiDoc(jsonData)
+    const md = genWecomApiDoc(schema)
     res.send({
-      schema: jsonData,
+      yaml: newYaml,
       md
     })
   } catch (e) {
     console.log(`---- ${operationid} schema解析失败----`)
     console.error(e)
     res.send({
-      schema: jsonData,
+      yaml: newYaml,
       md: {
         domStr: '',
         md: ''
