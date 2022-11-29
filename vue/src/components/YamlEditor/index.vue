a56274b33189f080c7d3f8eeda28d65876d899f1
caiozhang <caiozhang@tencent.com>
2022-11-29 16:58:10 +0800

    feat: 添加yaml注释

--- vue/src/components/YamlEditor/index.vue
+++ vue/src/components/YamlEditor/index.vue
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       yamlEditor: false,
-      value: window.jsyaml.dump(this.schema)
+      value: this.addComment(window.jsyaml.dump(this.schema))
     }
   },
   watch: {
@@ -56,7 +56,18 @@ export default {
   },
   methods: {
     getValue() {
-      return this.yamlEditor.getValue()
+      return this.addComment(this.yamlEditor.getValue())
+    },
+    addComment(yaml) {
+        let tempYaml = yaml
+        tempYaml = tempYaml.replace('summary:', '# 接口中文名\nsummary:')
+        tempYaml = tempYaml.replace('description:', '# 接口详细描述，将展示在接口文档顶部\ndescription:')
+        tempYaml = tempYaml.replace('permission:', '# 表示权限说明，将会显示在接口的基本信息表格中\npermission:')
+        tempYaml = tempYaml.replace('request:', '# 字段描述的基本类型(之后以baseType表示)：\n# - name: string\n#       example: string\n#       type: number 1(string)|2(array)|3(object)|4(number)|5(file)|6(boolean)\n#       description: 字段详细描述\n#       is_required: boolean true|false\n#       default: string\n# request类型：\n# auth 表示凭证参数\n# params 表示get请求的query参数\n# body 表示post请求的body参数\n# formData 表示附件上下传接口中的附件数据\nrequest:')
+        tempYaml = tempYaml.replace(/( +)items:/g, '#$1如果items字段type为2|3时需要补充items的内容，当type为2时，只有第一个baseType生效，格式如下:\n#$1properties:\n#$1      baseType\n$1items:')
+        tempYaml = tempYaml.replace('attention:', '# 注意事项，仅显示于接口的末尾\nattention:')
+        tempYaml = tempYaml.replace('response:', '# 以baseType的格式补充到body下\n# - name: string\n#       example: string\n#       type: number 1(string)|2(array)|3(object)|4(number)|5(file)|6(boolean)\n#       description: 字段详细描述\n#       is_required: boolean true|false\n#       default: string\nresponse:')
+        return tempYaml
     }
   }
 }
